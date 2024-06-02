dc3f4bd7c3d89f25dcb5832302a48588902ea872
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-02 23:44:50 +0530

    commit to add users to show

--- recommendations_streamlit.py
+++ recommendations_streamlit.py
@@ -1,8 +1,7 @@
-import streamlit as st
 import pandas as pd
 
 def search_user_ratings(user, attraction):
-    user_ratings_data = pd.read_csv('user_attractions_ratings.csv')
+    user_ratings_data = pd.read_csv('../datasets/user_attractions_ratings.csv')
     
     if user not in user_ratings_data.columns:
         return 0
@@ -15,24 +14,24 @@ def search_user_ratings(user, attraction):
         return 0
 
 def load_data():
-    attractions_data = pd.read_csv('final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description', 'Rating'])
-    user_ratings_data = pd.read_csv('user_attractions_ratings.csv')
+    attractions_data = pd.read_csv('../datasets/final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description', 'Rating'])
+    user_ratings_data = pd.read_csv('../datasets/user_attractions_ratings.csv')
     return attractions_data, user_ratings_data
 
 def load_predicted_data():
-    attractions_data = pd.read_csv('final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description', 'Rating'])
-    user_ratings_data = pd.read_csv('predicted_user_ratings.csv')
+    attractions_data = pd.read_csv('../datasets/final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description', 'Rating'])
+    user_ratings_data = pd.read_csv('../datasets/predicted_user_ratings.csv')
     return attractions_data, user_ratings_data
 
 def save_data(user_ratings_data):
-    user_ratings_data.to_csv('user_attractions_ratings.csv', index=False)
+    user_ratings_data.to_csv('../datasets/user_attractions_ratings.csv', index=False)
 
 def load_user_data(user):
-    attractions_data = pd.read_csv('final_attractions.csv', usecols=['Rating', 'Name', 'State', 'City', 'Country', 'Opening Hours', 'Description'])
-    if user not in pd.read_csv('predicted_user_ratings.csv').columns:
+    attractions_data = pd.read_csv('../datasets/final_attractions.csv', usecols=['Rating', 'Name', 'State', 'City', 'Country', 'Opening Hours', 'Description'])
+    if user not in pd.read_csv('../datasets/predicted_user_ratings.csv').columns:
         return None
 
-    user_ratings_data = pd.read_csv('predicted_user_ratings.csv', usecols=['Attraction', user])
+    user_ratings_data = pd.read_csv('../datasets/predicted_user_ratings.csv', usecols=['Attraction', user])
     attractions_data.rename(columns={'Rating': 'Google_Rating'}, inplace=True)
     attractions_data['User_Rating'] = user_ratings_data[user]
 
@@ -120,68 +119,3 @@ def get_recommendations(state, number_of_attractions, user):
         })
 
     return recommendations, message
-
-## streamlit
-def main_page():
-    st.markdown("<h1 style='text-align: center;'>Roamify</h1>", unsafe_allow_html=True)
-    st.sidebar.title('Navigation')
-    page = st.sidebar.radio('Go to', ['Home','Add User Ratings'])
-    if page == 'Add User Ratings':
-        add_user_streamlit_page()
-    elif page == 'Home':
-        recommendation_page()
-
-def recommendation_page():
-    attractions_data, user_ratings_data = load_predicted_data()
-    st.markdown("<h3 style='text-align: center;'>Tourist Attraction Recommendation System</h3>", unsafe_allow_html=True)
-
-    state = st.selectbox('Select State', attractions_data['State'].unique())
-
-    number_of_attractions = st.number_input('Number of Attractions', min_value=1, max_value=20, value=5, step=1)
-
-    user = st.text_input('Enter your name')
-
-    if st.button('Get Recommendations'):
-        recommendations, message = get_recommendations(state, number_of_attractions, user)
-        if recommendations == []:
-            st.write(message)
-        else:
-            st.write(f"Top {number_of_attractions} attractions in {state} for {user}:")
-            for rec in recommendations:
-                st.write(f"**Attraction name:** {rec['Name']}")
-                st.write(f"**City:** {rec['City']}")
-                st.write(f"**Opening Hours:** {rec['Opening Hours']}")
-                st.write(f"**Description:** {rec['Description']}")
-                st.write("***************************************************")
-
-    if st.checkbox('Show Raw Data'):
-        if user in user_ratings_data.columns:
-            st.write('Attractions and User Ratings Data')
-            user_ratings_data = load_user_data(user)
-            st.dataframe(user_ratings_data)
-        else:
-            st.warning(f"User {user} not found in the database. Please add user first.")
-
-def add_user_streamlit_page():
-    attractions_data, user_ratings_data = load_data()
-    st.markdown("<h3 style='text-align: center;'>Adding User Ratings</h3>", unsafe_allow_html=True)
-    state_ = st.selectbox('Select State', attractions_data['State'].unique())
-    new_user = st.text_input('Enter your name')
-
-    new_user_ratings = {}
-    available_attractions = attractions_data[attractions_data['State'] == state_]['Name'].tolist()
-    with st.form(key='ratings_form'):
-        st.write('Rate the attractions you have visited:')
-        for attraction in available_attractions:
-            existing_rating = float(search_user_ratings(new_user, attraction))
-            rating = st.slider(f'{attraction}', min_value=0.0, max_value=5.0, value=existing_rating, step=0.1, key=f'rating_{attraction}')
-
-            if rating > 0:
-                new_user_ratings[attraction] = rating
-    
-        if st.form_submit_button('Submit Ratings'):
-            add_user_ratings(new_user_ratings, new_user)
-            st.success('Ratings submitted successfully!')
-
-if __name__ == "__main__":
-    main_page()
